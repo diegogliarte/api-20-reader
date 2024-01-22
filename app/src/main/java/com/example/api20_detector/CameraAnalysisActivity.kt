e59c02cb5d1492070e20d262a45f8384292b2f46
Diego González Liarte <diegogliarte@gmail.com>
2024-01-22 18:30:32 +0100

    [REF] Stop photo when button pressed

--- app/src/main/java/com/example/api20_detector/CameraAnalysisActivity.kt
+++ app/src/main/java/com/example/api20_detector/CameraAnalysisActivity.kt
@@ -8,56 +8,82 @@ import android.widget.TextView
 import org.opencv.android.CameraActivity
 import org.opencv.android.CameraBridgeViewBase
 import org.opencv.android.OpenCVLoader
+import org.opencv.core.Mat
 import java.util.Collections
 
+
 class CameraAnalysisActivity : CameraActivity() {
-    private lateinit var cameraBridgeViewBase: CameraBridgeViewBase
     private var drawer = Drawer()
     private var cameraViewListener = CameraViewListener(drawer)
 
     private var isFirstSetAnalyzed = false
+    private var isAnalysing = false
     private val allMicrotubesColors = mutableListOf<List<MicrotubeColor>>()
 
     private val colorAnalyzer = ColorAnalyzer()
+
+    private val cameraBridgeViewBase by lazy { findViewById<CameraBridgeViewBase>(R.id.cameraView) }
+    private val cameraButton by lazy { findViewById<Button>(R.id.cameraButton) }
+    private val textView by lazy { findViewById<TextView>(R.id.numberOfMicrotubes) }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
-        cameraBridgeViewBase = findViewById(R.id.cameraView)
-        cameraBridgeViewBase.visibility = CameraBridgeViewBase.VISIBLE
-        cameraBridgeViewBase.setCvCameraViewListener(cameraViewListener)
+        setupCameraView()
+        cameraButton.setOnClickListener { cameraButtonListener() }
+    }
 
+    private fun setupCameraView() {
         if (OpenCVLoader.initDebug()) {
-            cameraBridgeViewBase.enableView()
+            cameraBridgeViewBase.apply {
+                visibility = CameraBridgeViewBase.VISIBLE
+                setCvCameraViewListener(cameraViewListener)
+                enableView()
+            }
         } else {
             Log.e("CameraActivity", "Unable to load OpenCV")
         }
+    }
 
-        val textView: TextView = findViewById(R.id.numberOfMicrotubes)
-        val cameraButton: Button = findViewById(R.id.cameraButton)
-        cameraButton.setOnClickListener {
-            cameraViewListener.getLatestFrame()?.let { frame ->
-                val microtubesRegions = drawer.getMicrotubesRegions(frame.width(), frame.height())
-                val analyzedColors = colorAnalyzer.analyzeMicrotubes(frame, microtubesRegions)
-                allMicrotubesColors.addAll(analyzedColors)
-
-                if (isFirstSetAnalyzed) {
-                    showAllMicrotubesColors()
-                } else {
-                    isFirstSetAnalyzed = true
-                    textView.text = "Microtubes 11 - 20"
-                }
+    private fun cameraButtonListener() {
+        cameraViewListener.getLatestFrame()?.let { frame ->
+            when {
+                !isAnalysing -> startAnalysis(frame)
+                !isFirstSetAnalyzed -> finishFirstAnalysis(frame)
+                else -> finishAnalysis(frame)
             }
         }
-
     }
+
+    private fun startAnalysis(frame: Mat) {
+        cameraViewListener.setFrameForAnalysis(frame)
+        isAnalysing = true
+    }
+
+    private fun finishFirstAnalysis(frame: Mat) {
+        val microtubesRegions = drawer.getMicrotubesRegions(frame.width(), frame.height())
+        val analyzedColors = colorAnalyzer.analyzeMicrotubes(frame, microtubesRegions)
+        allMicrotubesColors.addAll(analyzedColors)
+        isFirstSetAnalyzed = true
+        isAnalysing = false
+        cameraViewListener.setFrameForAnalysis(null)
+        textView.text = "Microtubes 11 - 20"
+    }
+
+    private fun finishAnalysis(frame: Mat) {
+        val microtubesRegions = drawer.getMicrotubesRegions(frame.width(), frame.height())
+        val analyzedColors = colorAnalyzer.analyzeMicrotubes(frame, microtubesRegions)
+        allMicrotubesColors.addAll(analyzedColors)
+        showAllMicrotubesColors()
+    }
+
 
     private fun showAllMicrotubesColors() {
         val intent = Intent(this, ManualAnalysisActivity::class.java)
 
-        allMicrotubesColors.forEachIndexed { index, microtubeColors  ->
+        allMicrotubesColors.forEachIndexed { index, microtubeColors ->
             val colorStrings = ArrayList(microtubeColors.map { it.toString() })
             intent.putStringArrayListExtra("microtube${index}Colors", colorStrings)
         }
@@ -68,9 +94,7 @@ class CameraAnalysisActivity : CameraActivity() {
         finish()
     }
 
-
     override fun getCameraViewList(): MutableList<out CameraBridgeViewBase> {
         return Collections.singletonList(cameraBridgeViewBase)
     }
-
 }
--- app/src/main/java/com/example/api20_detector/CameraViewListener.kt
+++ app/src/main/java/com/example/api20_detector/CameraViewListener.kt
@@ -1,34 +1,39 @@
 package com.example.api20_detector
 
 import org.opencv.android.CameraBridgeViewBase
-import org.opencv.core.Core
-import org.opencv.core.CvType
 import org.opencv.core.Mat
-import org.opencv.core.Point
-import org.opencv.core.Rect
-import org.opencv.core.Scalar
-import org.opencv.core.Size
-import org.opencv.imgproc.Imgproc
 
 class CameraViewListener(private val drawer: Drawer) : CameraBridgeViewBase.CvCameraViewListener2 {
-    private var latestFrame: Mat? = null
+    @Volatile private var latestFrame: Mat? = null
+    @Volatile private var frameForAnalysis: Mat? = null
 
-    fun getLatestFrame(): Mat? = latestFrame
+    fun getLatestFrame(): Mat? = synchronized(this) {
+        latestFrame?.clone()
+    }
 
+    fun setFrameForAnalysis(frame: Mat?) {
+        synchronized(this) {
+            frameForAnalysis?.release()
+            frameForAnalysis = frame?.clone()
+        }
+    }
 
     override fun onCameraViewStarted(width: Int, height: Int) {
+        // ...
     }
 
     override fun onCameraViewStopped() {
-        latestFrame?.release()
+        synchronized(this) {
+            latestFrame?.release()
+            frameForAnalysis?.release()
+        }
     }
 
     override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame): Mat {
-        val frame = inputFrame.rgba()
-
-        latestFrame?.release()
-        latestFrame = frame.clone()
-        return drawer.draw(frame)
+        synchronized(this) {
+            latestFrame?.release()
+            latestFrame = inputFrame.rgba()
+            return drawer.draw(latestFrame!!)
+        }
     }
-
 }
